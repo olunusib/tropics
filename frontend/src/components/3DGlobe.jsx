3477cfcd0c4f20a07eeb36e3d9d0c0eb09f00b66
Olunusi Best <olunusibest@gmail.com>
2023-02-11 16:30:37 -0600

    adds locations with data

--- frontend/src/components/3DGlobe.jsx
+++ frontend/src/components/3DGlobe.jsx
@@ -30,5 +30,4 @@ const World = () => {
     )
 };
 
-
 export default World;
--- frontend/src/views/LandingPage.jsx
+++ frontend/src/views/LandingPage.jsx
@@ -1,22 +1,16 @@
 import World from '../components/3DGlobe'
 import CSS from './LandingPage.module.css'
 
-
 const LandingPage = () => {
-
     return (
-
         <div >
             <h1>Welcome To Tropics</h1>
-
             <div className={CSS.globeContainer}>
             <World />
             </div>
-
         </div>
     )
 }
-
 
 export default LandingPage;
 
