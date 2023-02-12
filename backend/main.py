f4e9b134894238974cf109f17657d5ad5dc608c0
RobertTMZW7 <rmukuchura3696@myasu.alasu.edu>
2023-02-11 21:05:42 -0600

    adding date temps function

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,36 @@
 from flask import Flask
-
+import requests
+from datetime import datetime, timedelta
 api = Flask(__name__)
 
+lng , lat =  -71.0466, 42.3478
 @api.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Best",
-        "about" :"Auburn Hacks"
+def my_profile(lat, lng):
+
+    url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lng}&appid=5278a40f76dd0bf571b929f01a997887"
+
+    headers = {
+        "accept": "application/json",
+        "Accept-Encoding": "gzip"
     }
 
-    return response_body
+    response = requests.get(url, headers=headers)
+    mid_temps = {}
+    print(len(response.json()['list']))
+    temps = response.json()['list']
+    temps_index = [5, 13, 22, 28, 35]
+    for days, index in enumerate(temps_index):
+        mid_temps[datetime.now() + timedelta(days=days)] = {
+            "temp":temps[index]['main']['feels_like'],
+            "sea_level": temps[index]['main']['sea_level'],
+            "ground_level": temps[index]['main']['grnd_level'],
+            "humidity": temps[index]['main']['humidity']}
+        print("\n\n")
+    print(mid_temps)
+
+    return mid_temps
+
+my_profile(lat, lng)
+
+
+my_profile(lat, lng)
